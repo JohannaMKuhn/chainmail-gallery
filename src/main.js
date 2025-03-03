c16d813b1232ac6d9c89b4a9deaa70a92073f5e8
Johanna Kuhn <jkuhn5@cnm.edu>
2025-03-02 19:40:46 -0700

    added jewelry images to public directory

--- src/main.js
+++ src/main.js
@@ -1,2 +1,31 @@
 import './index.css'
 import 'flowbite'
+
+window.onload = function () {
+    initInputForm();
+}
+
+function initInputForm() {
+    let form = document.getElementById("item-form")
+
+    form.addEventListener("submit", (event) => {
+        handleItemForm(event, form)
+    })
+}
+
+function handleItemForm(event, formRef) {
+    if(event.preventDefault) {
+        event.preventDefault()
+    }
+
+    addInputToContactForm()
+    return false
+
+}
+
+function addInputToContactForm() {
+    let userName = document.getElementById("user-name")
+    let userEmail = document.getElementById("user-email")
+    let userMessage = document.getElementById("user-message")
+}
+
