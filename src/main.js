89df4489d1b790cedfacb557077147d307cfc18c
Johanna Kuhn <jkuhn5@cnm.edu>
2025-03-06 19:51:06 -0700

    installed zod; added frontend validation rules to main.js

--- src/main.js
+++ src/main.js
@@ -1,31 +1,23 @@
 import './index.css'
 import 'flowbite'
+import {z} from 'zod'
 
-window.onload = function () {
-    initInputForm();
+//Validation (frontend) rules in place.
+export function contactForm() {
+    const schema = z.object({})
+        name: z
+            .string({required_error: 'Name is a required field'})
+            .max(64, {message: 'Name cannot be greater than 64 characters'}),
+        email: z
+            .string({required_error: 'Email is a required field'})
+            .email({message: 'Invalid email address'})
+            .max(128, {message: 'Email cannot be greater than 128 characters'}),
+        subject: z
+            .string({})
+            .max(64, {message: 'Subject cannot be greater than 80 characters'})
+            .optional()
+        message: z
+            .string(required_error: 'Message is a required field')
+            .max(500, {message: 'Message cannot be greater than 500 characters'})
+
 }
-
-function initInputForm() {
-    let form = document.getElementById("item-form")
-
-    form.addEventListener("submit", (event) => {
-        handleItemForm(event, form)
-    })
-}
-
-function handleItemForm(event, formRef) {
-    if(event.preventDefault) {
-        event.preventDefault()
-    }
-
-    addInputToContactForm()
-    return false
-
-}
-
-function addInputToContactForm() {
-    let userName = document.getElementById("user-name")
-    let userEmail = document.getElementById("user-email")
-    let userMessage = document.getElementById("user-message")
-}
-
