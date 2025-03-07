467df546b1a3ba6da5e2098e6f71845e9f74338f
Johanna Kuhn <jkuhn5@cnm.edu>
2025-03-06 20:26:37 -0700

    grabbed form from html; grabbed input field elements for error indication; grabbed error display elements; grabbed status output element to display success status; defined success and error classes for feedbackto user of request's success

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import {z} from 'zod'
 
 //Validation (frontend) rules in place.
 export function contactForm() {
-    const schema = z.object({})
+    const schema = z.object({
         name: z
             .string({required_error: 'Name is a required field'})
             .max(64, {message: 'Name cannot be greater than 64 characters'}),
@@ -15,9 +15,30 @@ export function contactForm() {
         subject: z
             .string({})
             .max(64, {message: 'Subject cannot be greater than 80 characters'})
-            .optional()
+            .optional(),
         message: z
-            .string(required_error: 'Message is a required field')
+            .string({required_error: 'Message is a required field'})
             .max(500, {message: 'Message cannot be greater than 500 characters'})
+    })
+
+//Grab form to convert it into a form-data object and add event listeners
+    const form = document.getElementById('contact-form')
+
+//grab the required input fields so that we can add/remove a red border if an error occers
+    const nameInput = document.getElementById('user-name')
+    const emailInput = document.getElementById('user-email')
+    const messageInput = document.getElementById('user-message')
+
+//grab the error display elements to display error messages
+    const nameError = document.getElementById('nameError')
+    const emailError = document.getElementById('emailError')
+    const messageError = document.getElementById('messageError')
+
+//grab the status output element to show a success message on a successful submit or a backend error message
+    const statusOutput = document.getElementById('status')
+
+//define success and error classes to give user a quick visual hint on if the request was successful or not
+    const successClasses = ['text-green-800', 'bg-green-50']
+    const errorClasses = ['text-red-800', 'bg-red-50']
 
 }
